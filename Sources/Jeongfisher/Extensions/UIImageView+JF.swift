df5592ed2f6e875f6d8fcbddf6a297cec73ddf9c
유정주 <dbwjdwn9216@gmail.com>
2023-08-13 17:52:25 +0900

    Jeongfisher Quick Help 추가

--- Sources/Jeongfisher/Extensions/UIImageView+JF.swift
+++ Sources/Jeongfisher/Extensions/UIImageView+JF.swift
@@ -13,6 +13,7 @@ private struct JFAssociatedKeys {
 
 extension JeongfisherWrapper where Base: UIImageView {
     
+    /// UIImageView가 사용한 URL
     private var downloadUrl: String? {
         get {
             getAssociatedObject(base, &JFAssociatedKeys.downloadUrl)
@@ -22,13 +23,11 @@ extension JeongfisherWrapper where Base: UIImageView {
         }
     }
     
-    ///url을 이용해 UIImage 설정
-    ///
-    ///- Parameters:
-    ///     - url: 이미지 URL
-    ///     - placeHolder: 이미지 다운로드 지연 시 보여줄 placeHolder
-    ///     - watiPlaceHolderTime: placeHolde를 보여주기까지의 대기 시간
-    ///     - useCache: 캐시 사용 여부 결정. false면 네트워크를 통해 이미지 다운로드
+    /// - Parameters:
+    ///   - url: 이미지 URL
+    ///   - placeHolder: 다운로드 지연 시 보여줄 placeHolder 이미지
+    ///   - waitPlaceHolderTime: placeHolder 대기 시간
+    ///   - useCache: 캐시 사용 여부. false면 네트워크 다운로드
     public func setImage(
         with url: URL,
         placeHolder: UIImage? = nil,
@@ -56,6 +55,12 @@ extension JeongfisherWrapper where Base: UIImageView {
         }
     }
     
+    /// url 이미지를 가져오는 메서드
+    /// memory cache -> disk cache -> network 순서로 진행됨
+    /// - Parameters:
+    ///   - url: 이미지 URL
+    ///   - useCache: 캐시 사용 여부
+    /// - Returns: url 처리 결과
     private func fetchImage(with url: URL, useCache: Bool) async -> JFImageData? {
         if useCache,
             let jfImageData = await JFImageCache.shared.getImageWithCache(url: url) {
@@ -64,17 +69,16 @@ extension JeongfisherWrapper where Base: UIImageView {
             return try? await JFImageDownloader.shared.downloadImage(from: url)
         }
     }
-
+    
+    /// main thread에서 UIImageView 이미지 업데이트
+    /// - Parameter image: UIImageView에 넣을 UIImage
     private func updateImage(_ image: UIImage?) {
         DispatchQueue.main.async {
             self.base.image = image
         }
     }
     
-    ///진행 중인 URL 다운로드 취소
-    ///
-    ///- Parameters:
-    ///     - url: 이미지 URL
+    /// 진행 중인 다운로드 취소
     public func cancelDownloadImage() {
         guard let downloadUrlString = downloadUrl,
               let url = URL(string: downloadUrlString) else { return }
@@ -86,6 +90,12 @@ extension JeongfisherWrapper where Base: UIImageView {
 }
 
 private extension JeongfisherWrapper where Base: UIImageView {
+    /// PlaceHolder를 보여주는 타이머 생성.
+    /// 스크롤 도중에도 PlaceHolder를 보여줘야 하므로 DispatchQueue 사용
+    /// - Parameters:
+    ///   - placeHolder: 보여줄 PlaceHolder 이미지
+    ///   - waitTime: Place Holder 대기 시간
+    /// - Returns: Timer 반환
     func createPlaceHolderTimer(_ placeHolder: UIImage?, waitTime: TimeInterval) -> Timer? {
         guard let placeHolder = placeHolder else { return nil }
         
